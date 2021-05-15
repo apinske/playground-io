0e302ff55a414e06f8d8ecd416751f5e5960bbbb
Alexander Pinske <alex@pinske.eu>
2021-05-15 20:29:35 +0200

    Measure using JDK Base64-Decoder

--- src/main/java/Base64Test.java
+++ src/main/java/Base64Test.java
@@ -1,13 +1,15 @@
-
-
 import static java.lang.String.format;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
+import java.util.Base64;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.codec.binary.Base64InputStream;
 import org.apache.commons.codec.binary.Base64OutputStream;
@@ -18,6 +20,7 @@ public class Base64Test {
 
 	public static void main(String[] args) throws Exception {
 		File f = new File("target/data.b64");
+		long start = System.nanoTime();
 		if (args.length > 0 && "prepare".equals(args[0])) {
 			Random r = new Random(System.currentTimeMillis());
 			FileOutputStream fos = new FileOutputStream(f);
@@ -28,17 +31,23 @@ public class Base64Test {
 				bos.write(data);
 			}
 			bos.close();
-			bos = null;
 			fos.close();
-			fos = null;
-			data = null;
-		} else {
-			Base64InputStream is = new Base64InputStream(new FileInputStream(f));
-			long bytes = IOUtils.copyLarge(is, NullOutputStream.NULL_OUTPUT_STREAM);
-			System.out.println(format("%d KB", bytes / 1024));
-			for (GarbageCollectorMXBean gc : ManagementFactory.getGarbageCollectorMXBeans()) {
-				System.out.println(format("%s: %d", gc.getName(), gc.getCollectionCount()));
+		} else if (args.length > 0 && "jdk".equals(args[0])) {
+			for (int i = 0; i < 10; i++) {
+				InputStream is = Base64.getMimeDecoder().wrap(new BufferedInputStream(new FileInputStream(f)));
+				IOUtils.copy(is, NullOutputStream.NULL_OUTPUT_STREAM);
+				is.close();
 			}
+		} else {
+			for (int i = 0; i < 10; i++) {
+				Base64InputStream is = new Base64InputStream(new FileInputStream(f));
+				IOUtils.copy(is, NullOutputStream.NULL_OUTPUT_STREAM);
+				is.close();
+			}
+		}
+		System.out.println("took " + TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start) + "ms");
+		for (GarbageCollectorMXBean gc : ManagementFactory.getGarbageCollectorMXBeans()) {
+			System.out.println(format("%s: %d", gc.getName(), gc.getCollectionCount()));
 		}
 	}
 }
